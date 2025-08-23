d2b2cbde7c32b20e6324ba9f7579b1356388cac4
federico Chandia <fedelchandia@gmail.com>
2025-08-22 21:16:31 -0300

    Fix

--- src/interfaces/Sorteo.ts
+++ src/interfaces/Sorteo.ts
@@ -1,8 +1,6 @@
-import * as cheerio from 'cheerio';
-
 export interface Sorteo {
   numero: string,
   titulo: string,
   fecha: string,
-  link: cheerio.Element | string | undefined,
+  link: any | string | undefined,
 }
