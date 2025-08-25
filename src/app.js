7d4b2923e2acb5a77d35fd84adc7a35f4ccc2fd1
federico Chandia <fedelchandia@gmail.com>
2025-08-25 19:48:29 -0300

    fix: Javascript puro

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const middlewares = require('./middlewares');
+const indexRouter = require('./routes/index');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.use('/', indexRouter);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+module.exports = app;
